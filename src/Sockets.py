55809d5e5e2ce08558992aa1522c94c32a2b7864
whoadarshpandya <adarshpandya0@gmail.com>
2021-05-02 10:53:51 +0530

    final builf

--- src/Sockets.py
+++ src/Sockets.py
@@ -1,21 +1,40 @@
 from fastapi import WebSocket, WebSocketDisconnect
 from typing import List
+import json
 
 
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
+        self.friends_online = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, client_id: str, name: str):
         await websocket.accept()
         self.active_connections.append(websocket)
+        self.friends_online.append({"client_id": client_id, "name": name})
 
-    def disconnect(self, websocket: WebSocket):
+    def disconnect(self, websocket: WebSocket, client_id: str):
         self.active_connections.remove(websocket)
+        self.friends_online = [
+            i for i in self.friends_online if not (i['client_id'] == client_id)]
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    # async def send_personal_message(self,websocket: WebSocket):
+    async def send_personal_message(self, message: dict, client_id: str):
+        # print("cin")
+        # print(self.active_connections)
+        await websocket.send_text(json.dumps(message))
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+    async def broadcast(self, message: dict):
+        # await print(message['client_id'])
+        # await print(message['msg'])
+        if message['event'] == "friends":
+            for connection in self.active_connections:
+                await connection.send_text(json.dumps(message))
+        if message['event'] == "chat":
+            await self.active_connections[message['order']['one']].send_text(json.dumps(message))
+            await self.active_connections[message['order']['two']].send_text(json.dumps(message))
+        # for connection in self.active_connections:
+        # await connection.send_text(json.dumps(message))
+
+    def sendFriends(self):
+        return self.friends_online
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from typing import Optional
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from .Sockets import ConnectionManager
+import json
 
 app = FastAPI()
 origins = ["*"]
@@ -16,63 +17,37 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-print("hello")
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
 
 
 @app.get('/')
 def root():
-    return HTMLResponse(content=html, status_code=200)
+    return {'msg': 'socket io api is working...'}
 
 
 manager = ConnectionManager()
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+async def findIndex(client_id):
+    fri = next((index for (index, d) in enumerate(
+        manager.friends_online) if d["client_id"] == client_id), None)
+    print("found")
+    print(fri)
+    return fri
 
-    await manager.connect(websocket)
+
+@app.websocket("/ws/{client_id}/{name}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str, name: str):
+    print(manager.sendFriends())
+    await manager.connect(websocket, client_id, name)
     try:
         while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast({"event": "friends", "friends": manager.sendFriends()})
+            data = json.loads(await websocket.receive_text())
+            # await manager.send_personal_message("adjskl")
+            # print(data)
+            index = await findIndex(data['id'])
+            my_indx = await findIndex(client_id)
+            await manager.broadcast({"event": "chat", "order": {'one': index, 'two': my_indx}, "client_id": client_id, "msg": data['msg']})
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        print("initiate leaving sequence")
+        manager.disconnect(websocket, client_id)
